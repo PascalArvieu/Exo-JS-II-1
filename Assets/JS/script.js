993c13f4e298661a08bcab93f79e470cbbca0809
PascalArvieu <pascalarvieu@gmail.com>
2020-11-04 10:43:01 +0100

    JS Exo UU-1

---
+++ Assets/JS/script.js
@@ -0,0 +1,10 @@
+//Exercice II - 1
+
+  function skull() {
+    document.images["trump"].src="Assets/Images/Trump2.jpg"
+    }
+    
+  function head() {
+    document.images["trump"].src="Assets/Images/Trump1.jpg"
+    }
+
